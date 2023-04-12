8b016c3c64b752a9b30159dd426d7b3a4d8be80c
srushikeshs <srushikesh@utexas.edu>
2023-04-11 19:57:04 -0500

    Added wind component

--- SIRIYADURAI_HWK_3_OPT.py
+++ SIRIYADURAI_HWK_3_OPT.py
@@ -1,48 +1,48 @@
-"""
-Optimization question:
-What is the least cost optimal mix of wind, solar, and energy storage system (ESS) to deliver a constant amount of power demand?  
-"""
 
+## Importing Library
 from __future__ import division
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
 
 ## constants and assumptions
 # capital costs for solar, and energy storage systems
-solar_cap_cost 			= 800       # $/kW
-ESS_p_cap_cost 			= 200       # $/kW
-ESS_e_cap_cost 			= 150       # $/kWh
+solar_cap_cost = 800000000
+wind_cap_cost = 1200000000      
+ESS_p_cap_cost = 200000000       
+ESS_e_cap_cost = 150000000       
 
 # energy storage operational assumptions
-ESS_min_level    		= 0.20      # %, minimum level of discharge of the battery
-ESS_eta_c           	= 0.95      # ESS charging efficiency, looses 5% when charging
-ESS_eta_d        		= 0.9       # ESS discharging efficiency, looses 10% when discharging
-ESS_p_var_cost          = 0.005     # ESS discharge cost $/kWh
-
-curtailment_cost        = 0.001     # curtailment penalty $/kWh
-
-demand                  = 1000      # kW, how much power must the system deliver?
+ESS_min_level = 0.20      
+ESS_eta_c = 0.95      
+ESS_eta_d = 0.9       
+ESS_p_var_cost = 5000     
+curtailment_cost = 1000     
 
 # create the model
-model = AbstractModel(name = 'solar-storage model')
+model = AbstractModel(name = 'energy storage model')
 
 # create model sets
-model.t                 = Set(initialize = [i for i in range(8760)], ordered=True)    
-model.tech              = Set(initialize =['s_cap', 'ESS_power_cap', 'ESS_energy_cap'], ordered=True)  
+model.t = Set(initialize = [i for i in range(8760)], ordered=True)    
+model.tech = Set(initialize =['s_cap', 'w_cap', 'ESS_power_cap', 'ESS_energy_cap'], ordered=True)  
 
-model.solar             = Param(model.t)
-model.costs             = Param(model.tech, initialize={'s_cap' : solar_cap_cost, 'ESS_power_cap' : ESS_p_cap_cost, 'ESS_energy_cap' : ESS_e_cap_cost})
+# parameters
+model.solar = Param(model.t)
+model.wind = Param(model.t)
+model.demand = Param(model.t)
+model.costs = Param(model.tech, initialize={'s_cap' : solar_cap_cost, 'w_cap' : wind_cap_cost, 'ESS_power_cap' : ESS_p_cap_cost, 'ESS_energy_cap' : ESS_e_cap_cost})
 
 ## load data into parameters, solar and wind data are houlry capacity factor data
 data = DataPortal()
-data.load(filename = 'opt_model_data/wind_solar_cf.csv', select = ('t', 'solar'), param = model.solar, index = model.t)
+data.load(filename = 'opt_model_data/2022_ERCOT_data.csv', select = ('t', 'solar'), param = model.solar, index = model.t)
+data.load(filename = 'opt_model_data/2022_ERCOT_data.csv', select = ('t', 'wind'), param = model.wind, index = model.t)
+data.load(filename = 'opt_model_data/2022_ERCOT_data.csv', select = ('t', 'demand'), param = model.demand, index = model.t)
 
 ## define variables
-model.cap               = Var(model.tech, domain = NonNegativeReals)
-model.ESS_SOC           = Var(model.t, domain = NonNegativeReals)
-model.ESS_c             = Var(model.t, domain = NonNegativeReals)
-model.ESS_d             = Var(model.t, domain = NonNegativeReals)
-model.curt              = Var(model.t, domain = NonNegativeReals)
+model.cap = Var(model.tech, domain = NonNegativeReals)
+model.ESS_SOC = Var(model.t, domain = NonNegativeReals)
+model.ESS_c = Var(model.t, domain = NonNegativeReals)
+model.ESS_d = Var(model.t, domain = NonNegativeReals)
+model.curt = Var(model.t, domain = NonNegativeReals)
 
 # define objective function and contraints
 
@@ -53,7 +53,7 @@ model.OBJ = Objective(rule=obj_expression)
 
 # supply/demand match constraint
 def match_const(model, i):
-    return model.solar[i]*model.cap['s_cap'] + model.ESS_d[i] - model.ESS_c[i] - model.curt[i] - demand == 0   
+    return model.solar[i]*model.cap['s_cap'] + model.wind[i]*model.cap['w_cap'] + model.ESS_d[i] - model.ESS_c[i] - model.curt[i] - model.demand[i] == 0   
 model.match = Constraint(model.t, rule = match_const)
 
 # ESS charge/discharge constraint
@@ -78,12 +78,8 @@ def SOC_const(model, i):
     return model.ESS_SOC[i] == model.ESS_SOC[i-1] + (model.ESS_c[i] * ESS_eta_c) - (model.ESS_d[i]/ESS_eta_d) 
 model.SOC_const = Constraint(model.t, rule = SOC_const)
 
-
 # create instance of the model (abstract only)
 model = model.create_instance(data)
-
-# look at model attributes
-# model.t.pprint()
 
 # solve the model
 opt = SolverFactory('glpk')
@@ -91,4 +87,4 @@ status = opt.solve(model)
 
 # write model outputs to a JSON file
 model.solutions.store_to(status)
-status.write(filename='solar_storage.json', format='json')
+status.write(filename='SIRIYADURAI_HWK3_OPT_OUTPUTS.json', format='json')
